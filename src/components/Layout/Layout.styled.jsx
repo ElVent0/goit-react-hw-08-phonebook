f07afa047e66262e386184e7397ad01d6559ca44
Vasyl Tarasyuk <vasfreelan@gmail.com>
2022-12-04 20:52:35 +0200

    1.4

--- src/components/Layout/Layout.styled.jsx
+++ src/components/Layout/Layout.styled.jsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 
 export const LayoutStyled = styled.div`
-  height: 100vh;
+  height: 100%;
+  min-height: 100vh;
   background: linear-gradient(90deg, #2d2d2d 2.54%, #545454);
 `;
